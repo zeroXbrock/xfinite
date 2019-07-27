78ccd6b582dc08c16d0adfd281d7ed8b80763b58
Brock Smedley <2791467+BrockSmedley@users.noreply.github.com>
2019-07-27 01:14:11 -0700

    post result to twitter

---
+++ lib.py
@@ -0,0 +1,60 @@
+import speedtest
+import settings
+import twitter
+
+api = twitter.Api(
+    consumer_key=settings.API_KEY,
+    consumer_secret=settings.API_SECRET,
+    access_token_key=settings.ACCESS_TOKEN,
+    access_token_secret=settings.ACCESS_SECRET
+)
+expectDown = float(settings.EXPECTED_DOWN_MBPS)
+expectUp = float(settings.EXPECTED_UP_MBPS)
+
+# returns string-formatted percent delta (+X or -X)
+def deltaStr(percentLoss):
+    percentLoss *= -1.0
+    if (percentLoss < 0):
+        return str(percentLoss)
+    else:
+        return "+" + str(percentLoss)
+
+# returns tweet-formatted result
+def formatResult(result):
+    dl = round(result['download'] / 1000000.0, 1)
+    ul = round(result['upload'] / 1000000.0, 1)
+    pg = round(result['ping'], 1)
+
+    download = str(dl) + " Mbps"
+    upload = str(ul) + " Mbps"
+    ping = str(pg) + " ms"
+    ip = result['client']['ip']
+    img = result['share']
+
+    lossDown = float(expectDown) - dl
+    lossUp = float(expectUp) - ul
+
+    percentLossDown = deltaStr((lossDown / expectDown) * 100)
+    percentLossUp = deltaStr((lossUp / expectUp) * 100)
+
+    return """Paid for {0} down, {1} up.
+    Down: {2} ({3}%)
+    Up: {4} ({5}%)
+    Ping: {6}
+    IP: {7}""".format(expectDown, expectUp, download, percentLossDown, upload, percentLossUp, ping, ip)
+
+def testSpeed():
+    print("testing speed...")
+    s = speedtest.Speedtest()
+    s.get_best_server()
+    s.download()
+    s.upload()
+    s.results.share()
+
+    return s.results.dict()
+
+
+def postTweet(text):
+    print("posting tweet...")
+    result = api.PostUpdate(text)
+    print("tweet result: " + str(result))
--- main.py
+++ main.py
@@ -1,40 +1,12 @@
-import speedtest
-import settings
-
-
-# {'client':
-#   {'rating': '0', 'loggedin': '0', 'isprating': '3.7',
-#     'ispdlavg': '0', 'ip': '67.189.31.203', 'isp': 'Comcast Cable',
-#     'lon': '-123.1008', 'ispulavg': '0', 'country': 'US', 'lat': '44.0197'
-#   },
-#   'bytes_sent': 15769600, 'download': 76905445.7852311,
-#   'timestamp': '2019-07-27T06:10:50.236057Z',
-#   'share': u'http://www.speedtest.net/result/8450775228.png',
-#   'bytes_received': 96571756, 'ping': 28.591, 'upload': 12193065.659968669,
-#   'server': {
-#     'latency': 28.591, 'name': 'Corvallis, OR',
-#     'url': 'http://speedtest.peakinternet.com:8080/speedtest/upload.php',
-#     'country': 'United States', 'lon': '-123.2760', 'cc': 'US',
-#     'host': 'speedtest.peakinternet.com:8080', 'sponsor': 'PEAK Internet',
-#     'url2': 'http://speedtest2.peakinternet.com/speedtest/upload.php',
-#     'lat': '44.5708', 'id': '1159', 'd': 62.845870823467436
-#   }}
-def testSpeed():
-    s = speedtest.Speedtest()
-    s.get_best_server()
-    s.download()
-    s.upload()
-    s.results.share()
-
-    return s.results.dict()
-
+from lib import *
 
 def main():
-    print("secret: " + str(settings.SECRET_TEST))
     result = testSpeed()
-    print("download: " + str(result['download'] / 1000000.0) + "Mbps")
-    print("upload: " + str(result['upload'] / 1000000.0) + "Mbps")
-    print("ping: " + str(result['ping']))
+    tweet = formatResult(result)
+
+    print tweet
+    if (len(tweet) <= 140):
+        postTweet(tweet)
 
 
 if (__name__ == "__main__"):
