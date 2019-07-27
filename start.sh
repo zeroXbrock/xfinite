e8c307848fd741464e3270bc2067abf0373e7640
Brock Smedley <2791467+BrockSmedley@users.noreply.github.com>
2019-07-27 01:23:04 -0700

    Create start.sh

---
+++ start.sh
@@ -0,0 +1,17 @@
+#!/bin/bash
+
+echo "installing dependencies..."
+python --version &>/dev/null
+if [ "$?" != "0" ]; then
+    echo "please install python 2.7"
+    exit 1
+fi
+
+pip --version &>/dev/null
+if [ "$?" != "0" ]; then
+    echo "please install pip for python 2.7"
+    exit 1
+fi
+
+pip install -r requirements.txt &>/dev/null
+python main.py
