43ac4cbe3fc79c0a97a6a9358d00fac9eb8c8341
Brock Smedley <2791467+BrockSmedley@users.noreply.github.com>
2019-07-26 23:48:41 -0700

    Initial commit

---
+++ settings.py
@@ -0,0 +1,9 @@
+# settings.py
+import os
+from dotenv import load_dotenv
+load_dotenv()
+
+API_KEY = os.getenv("API_KEY")
+API_SECRET = os.getenv("API_SECRET")
+ACCESS_TOKEN = os.getenv("ACCESS_TOKEN")
+ACCESS_SECRET = os.getenv("ACCESS_SECRET")
